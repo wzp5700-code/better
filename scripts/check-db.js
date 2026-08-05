50eb6fbd0ce25ba23e58831ee8eb4ecb26846207
wzp5700-code <wzp5700-code@users.noreply.github.com>
2026-08-05 11:23:23 +0800

    chore: remove temp debug script check-db.js (accidentally committed in 619cee0)

--- scripts/check-db.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const Db = require("/app/node_modules/.pnpm/better-sqlite3@13.0.2/node_modules/better-sqlite3")
-const db = new Db("/app/data/app.db")
-for (const table of ["habits", "journal_entries", "books", "devices"]) {
-  const r = db.prepare(`SELECT COUNT(*) c FROM ${table}`).get()
-  console.log(table, r.c)
-}
-db.close()
